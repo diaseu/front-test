a097d352e626949a3f8f1a67456f7bb3855fa29c
diaseu <diana.seung@gmail.com>
2021-11-15 15:04:29 -0800

    match

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="icon">+</div>
+  <div class="icon"></div>
 </template>
 
 <script>
@@ -10,15 +10,14 @@ export default {
 
 <style scoped>
 .icon {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: block;
   border-radius: 50%;
   width: 48px;
   height: 48px;
+  background-image: url('https://i.imgur.com/WFqSBgc.png');
   background-color: rgba(20,20,20,0.3);
   background-position: 50% 0;
   background-size: auto 125%;
-  margin: 12px auto;
+  /* margin: 12px auto; */
 }
 </style>
--- src/views/Match.vue
+++ src/views/Match.vue
@@ -4,24 +4,45 @@
     <h1>Match</h1>
     <n-button>naive-ui</n-button>
 
-    <n-card title="NSE Tropical (New Match)" header-style="background-color: rgba(250,50,50,0.2);" content-style="background-color: rgba(250,50,50,0.2);">
-        <!-- <template #cover>
-          <Icon />
-        </template> -->
-        Recently active, match now!
-    </n-card>
-    <n-card title="Ken's Philodendron">
-        <!-- <template #cover>
-          <Icon />
-        </template> -->
-        Click to match now!
-    </n-card>
-    <n-card title="Mickey's Nursery">
-        <!-- <template #cover>
-          <Icon />
-        </template> -->
-        Click to match now!
-    </n-card>
+    <div class="matched active">
+      <div class="icon">
+        <Icon />
+      </div>
+      <div class="card">
+          <h3>NSE Tropical</h3>
+          <span>
+            Recently active, match now!
+          </span>
+      </div>
+          <!-- <template #cover>
+          
+          </template> -->
+
+    </div>
+
+    <div class="matched">
+      <div class="icon">
+        <Icon />
+      </div>
+      <n-card title="Golden Communications">
+          <!-- <template #cover>
+            <Icon />
+          </template> -->
+          Click to match now!
+      </n-card>
+    </div>
+
+    <div class="matched">
+      <div class="icon">
+        <Icon />
+      </div>
+      <n-card title="Mojo Solutions">
+          <!-- <template #cover>
+            <Icon />
+          </template> -->
+          Click to match now!
+      </n-card>
+    </div>
   </div>
 
 
@@ -43,18 +64,41 @@
 </script>
 
 <style scoped>
+h3 {
+  margin: 0;
+}
   .match {
     width: 100%;
     display: flex;
     flex-direction: column;
     padding: 20px;
   }
-  .n-card {
+  .card {
     width: 100%;
-    background-color: rgba(0,0,0,0.1);
     display: block;
     text-align: left;
     margin-bottom: 10px;
+    background-color: transparent;
+    border: 0;
+    padding: 12px;
+  }
+  .n-card__content {
+    padding-top: 0;
+  }
+
+  .matched {
+    display: flex;
+    flex-direction: row;
+    background-color: rgba(0,0,0,0.0);
+    margin-bottom: 12px;
+    border-bottom: 1px solid rgba(0,0,0,0.5);
+  }
+
+  .icon {
+    /* display: inline-block; */
+    padding: 8px 12px;
+    margin: -1px;
+    /* flex-grow: 0; */
   }
   .active {
     background-color: rgba(250,50,50,0.3);
